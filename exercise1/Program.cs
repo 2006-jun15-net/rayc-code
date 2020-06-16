c75fd9627beafa17b2db49b37f37abe4f9630d51
Vocalisms <raymond.m.casco@gmail.com>
2020-06-16 18:00:43 -0400

    first exercise completed

---
+++ exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] staircase = new String[8];
+
+            for (int i = 0; i<staircase.Length; i++)
+            {
+                staircase[i] = " ";
+            }
+
+
+                for(int j = 7; j >= 0; j--)
+                {
+                    staircase[j] = "#";
+
+                    for (int k = 0; k < 8; k++){
+                          Console.Write(staircase[k]);
+                    }
+                    Console.WriteLine("");
+                }
+        }
+    }
+}
