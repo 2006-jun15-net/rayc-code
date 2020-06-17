2d1cfc5be74f49d3874cfe0dafea23d13b677ea3
Vocalisms <raymond.m.casco@gmail.com>
2020-06-16 23:40:36 -0400

    not sure what im committing

--- 1-csharp/ProceduralBasics/Program.cs
+++ 1-csharp/ProceduralBasics/Program.cs
@@ -6,6 +6,11 @@ namespace ProceduralBasics
     {
         static void Main(string[] args)
         {
+            //the Main rmethod is ultimately run by the "dotnet run" command. 
+
+            //each "line of code" is a "statement" ending in a semicolon.
+            int x = 5; //declares a variable named "x' with type "int" (integer, whole number), and 
+                       // gives it the value 5.
             Console.WriteLine("Hello World!");
         }
     }
