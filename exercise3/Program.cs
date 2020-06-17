db65bf75ff4a0bd8aa831983218cea24128258ee
Vocalisms <raymond.m.casco@gmail.com>
2020-06-17 01:03:20 -0400

    Exercise 3 Completed

---
+++ exercise3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            while(number > 1)
+            {
+                if(number%2 == 0)
+                {
+                    number = number/2;
+                    Console.WriteLine(number);
+                }else
+                {
+                    number = ((number*3) +1);
+                    Console.WriteLine(number);
+                }
+
+            }
+        }
+    }
+}
